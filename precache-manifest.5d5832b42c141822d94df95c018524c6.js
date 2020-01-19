d0b1b0bb85a669fc42798bbff0e3ab06e44fd3b3
tootie29 <chardmedina@gmail.com>
2020-01-19 19:38:48 +0800

    Updates

---
+++ precache-manifest.5d5832b42c141822d94df95c018524c6.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a200089985021232d107ed3b3feb7f",
+    "url": "/dnar/index.html"
+  },
+  {
+    "revision": "a36dda6aa91d2b1b4d48",
+    "url": "/dnar/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "48616ff66da53493c24c",
+    "url": "/dnar/static/css/main.8532bba9.chunk.css"
+  },
+  {
+    "revision": "a36dda6aa91d2b1b4d48",
+    "url": "/dnar/static/js/2.151d79b4.chunk.js"
+  },
+  {
+    "revision": "839d8c843381c8c34e659d25c38155ca",
+    "url": "/dnar/static/js/2.151d79b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "48616ff66da53493c24c",
+    "url": "/dnar/static/js/main.425348f9.chunk.js"
+  },
+  {
+    "revision": "2041ac39227e82b8a57c",
+    "url": "/dnar/static/js/runtime-main.84fad61f.js"
+  },
+  {
+    "revision": "0d962d6080729d609c8a919b325aa8bf",
+    "url": "/dnar/static/media/DANGER.0d962d60.png"
+  },
+  {
+    "revision": "0b7809bdc5cb332dc7720f8b812c3b54",
+    "url": "/dnar/static/media/Helvetica-Bold-Font.0b7809bd.ttf"
+  },
+  {
+    "revision": "aa821a5c62e7f01a8e874f0810a55e6b",
+    "url": "/dnar/static/media/Helvetica-Light-Oblique-font.aa821a5c.ttf"
+  },
+  {
+    "revision": "fa1b8881761156182ed76ae1dbcdf430",
+    "url": "/dnar/static/media/PIC-1.fa1b8881.png"
+  },
+  {
+    "revision": "3444913c3d5072ff365bb019886c1442",
+    "url": "/dnar/static/media/PIC-2.3444913c.png"
+  },
+  {
+    "revision": "3b2105b13b6e13b571c24478d30c20f2",
+    "url": "/dnar/static/media/PIC-3.3b2105b1.png"
+  },
+  {
+    "revision": "5ab80be3ed8fb062166c9da882379824",
+    "url": "/dnar/static/media/PIC-4.5ab80be3.png"
+  },
+  {
+    "revision": "40ef2e85375dc8f016a7672750af8871",
+    "url": "/dnar/static/media/PIC-TESTIMONI-1.40ef2e85.png"
+  },
+  {
+    "revision": "8e1683d32f0adca39f7eaf7bbf0b1f4b",
+    "url": "/dnar/static/media/PIC-TESTIMONI-2.8e1683d3.png"
+  },
+  {
+    "revision": "5cbede00c5bd268cc043bbc4aaf6ac8c",
+    "url": "/dnar/static/media/PIC-TESTIMONI-3.5cbede00.png"
+  },
+  {
+    "revision": "75ffc8eb5c9defdb7d2b61ee7a79fc7a",
+    "url": "/dnar/static/media/SIJIL-KKM-1.75ffc8eb.png"
+  },
+  {
+    "revision": "f19a0d93f48a552e9afe22df34c38aa0",
+    "url": "/dnar/static/media/SIJIL-KKM-2.f19a0d93.png"
+  },
+  {
+    "revision": "2c58771faa59dd8112537b59b693c185",
+    "url": "/dnar/static/media/SIJIL-KKM-3.2c58771f.png"
+  },
+  {
+    "revision": "1e78275c4b4a9c0ecd36ab71538e5de4",
+    "url": "/dnar/static/media/SIJIL-KKM-4.1e78275c.png"
+  },
+  {
+    "revision": "d632ff9bcec987264c31c27b2afaa8e6",
+    "url": "/dnar/static/media/SIJIL-KKM-5.d632ff9b.png"
+  },
+  {
+    "revision": "778310361755a819664aca4101d9a73a",
+    "url": "/dnar/static/media/STAR.77831036.png"
+  },
+  {
+    "revision": "68866f038c78ada762504bf998fe2de0",
+    "url": "/dnar/static/media/helvetica_light.68866f03.ttf"
+  },
+  {
+    "revision": "496a8d64bc537842a674593e2b545df6",
+    "url": "/dnar/static/media/img-1.496a8d64.png"
+  },
+  {
+    "revision": "32693340cd00c8496cf725123e643523",
+    "url": "/dnar/static/media/question-mark.32693340.png"
+  }
+]);
